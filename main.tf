6b6dde05bca214c2db3e0b0cf5f38dd1b40541a8
vinaykumar331 <vinaymech331@gmail.com>
2023-01-06 15:40:38 +0530

    modified tfvars

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 #This Terraform Code Deploys Basic VPC Infra.
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 950788596da4305a2109f656a13a7e92414cb744
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
